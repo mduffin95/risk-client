87495eef2acdbce4e936294859ce0c52bc869f7a
Matthew Duffin <mduffin95@users.noreply.github.com>
2021-12-23 23:17:51 +0000

    First commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container fill-height>
+    <div class="map-container">
+      <img
+        class="map"
+        alt="Risk board"
+        src="https://upload.wikimedia.org/wikipedia/commons/4/4a/Risk_board.svg"
+      />
+      <Marker
+        v-for="(territory, index) in territories"
+        :key="'marker-' + index"
+        :territory="territory"
+      />
+    </div>
+
+  </v-container>
+</template>
+
+<script>
+import Marker from "./components/Marker.vue";
+
+export default {
+  name: "App",
+  components: {
+    Marker,
+  },
+  data() {
+    return {
+      territories: [
+        {
+          top: 50,
+          left: 50,
+          units: 1,
+          color: "red"
+        }
+        ],
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.map-container {
+    position: relative;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 80%;
+}
+.map {
+    width: 100%;
+}
+</style>
---
+++ src/components/Marker.vue
@@ -0,0 +1,37 @@
+<template>
+  <div :id="territory.name"
+    :style="'top: ' + territory.top + '%; left: ' + territory.left + '%; border-color: ' + territory.color"
+    class="circle option"
+  >{{ territory.units }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Marker',
+  props: {
+    territory: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.circle {
+    position: absolute;
+    border-radius: 50%;
+    text-align: center;
+    vertical-align: middle;
+    width: 3vw;
+    height: 3vw;
+    line-height: calc(3vw - 6px);
+    font-size: 1vw;
+    border: 3px solid;
+}
+.option {
+    background: white;
+}
+
+.optionSelected {
+    background: cyan;
+}
+</style>
