4bdf3330dbe55e55e01e83261b8c149d09980104
Matthew Duffin <mduffin95@users.noreply.github.com>
2022-11-16 23:19:30 +0000

    persistent state

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import Oruga from '@oruga-ui/oruga-next'
 import '@oruga-ui/oruga-next/dist/oruga.css'
 import App from './App.vue'
 import router from './router'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 
 const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
 createApp(App)
   .use(pinia)
   .use(Oruga)
--- src/stores/GameStore.js
+++ src/stores/GameStore.js
@@ -1,8 +1,14 @@
 import { defineStore } from "pinia";
 
-export const useGameStore = defineStore("game", () => {
-    const playerName = null;
-    const model = {}
-    const lastSelected = {}
-    return { playerName, model, lastSelected }
+export const useGameStore = defineStore("game", {
+    state: () => {
+        const playerName = null;
+        const model = {}
+        const lastSelected = {}
+        return { playerName, model, lastSelected }
+    },
+    persist: {
+        storage: sessionStorage,
+        paths: ['playerName'],
+    },
 });
